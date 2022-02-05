a26f383de6c9b31d0e7acb5034ca598453954de1
Viacheslav Slinko <vslinko@yahoo.com>
2022-02-05 16:30:19 +0300

    WIP

--- jest/global-teardown.js
+++ jest/global-teardown.js
@@ -1,12 +1,28 @@
 const fs = require("fs");
 const debug = require("debug")("jest-obsidian");
 
-module.exports = () => {
+module.exports = async () => {
+  const p = new Promise(resolve => {
+    obsidian.once("exit", resolve);
+  });
+
   if (global.wss) {
     global.wss.close();
   }
   if (global.obsidian) {
     global.obsidian.kill();
+  }
+
+  await p;
+
+  if (global.originalTestFile) {
+    debug(`Restoring ${TEST_FILE_PATH}`);
+    fs.writeFileSync(TEST_FILE_PATH, originalTestFile);
+  }
+
+  if (global.originalVaultConfig) {
+    debug(`Restoring ${VAULT_CONFIG_PATH}`);
+    fs.writeFileSync(VAULT_CONFIG_PATH, originalVaultConfig);
   }
 
   if (global.originalObsidianConfig) {
