e6fa6e69f12ae058214214d68a162e1d6995debb
Viacheslav Slinko <me@vslinko.com>
2023-04-20 10:16:31 +0300

    DND handle cursor movement

--- src/operations/MoveListToDifferentPositionOperation.ts
+++ src/operations/MoveListToDifferentPositionOperation.ts
@@ -31,6 +31,8 @@ export class MoveListToDifferentPositionOperation implements Operation {
     this.stopPropagation = true;
     this.updated = true;
 
+    const cursorAnchor = this.calculateCursorAnchor();
+
     this.listToMove.getParent().removeChild(this.listToMove);
 
     if (this.whereToMove === "before") {
@@ -45,12 +47,48 @@ export class MoveListToDifferentPositionOperation implements Operation {
     );
     this.listToMove.indentContent(0, this.placeToMove.getFirstLineIndent());
 
+    if (cursorAnchor) {
+      const cursorListStart =
+        cursorAnchor.cursorList.getFirstLineContentStart();
+
+      this.root.replaceCursor({
+        line: cursorListStart.line + cursorAnchor.lineDiff,
+        ch: cursorListStart.ch + cursorAnchor.chDiff,
+      });
+    }
+
     // handle different indent levels
-    // handle cursor
     // handle mouse cursor image
     // handle when list have fold/unfold
     // try to draw pretty box
+    // handle when root is changed while moving
+    // some e2e tests?
 
     recalculateNumericBullets(this.root);
+  }
+
+  private calculateCursorAnchor() {
+    const cursorLine = this.root.getCursor().line;
+
+    const lines = [
+      this.listToMove.getFirstLineContentStart().line,
+      this.listToMove.getLastLineContentEnd().line,
+      this.placeToMove.getFirstLineContentStart().line,
+      this.placeToMove.getLastLineContentEnd().line,
+    ];
+    const listStartLine = Math.min(...lines);
+    const listEndLine = Math.max(...lines);
+
+    if (cursorLine < listStartLine || cursorLine > listEndLine) {
+      return null;
+    }
+
+    const cursor = this.root.getCursor();
+    const cursorList = this.root.getListUnderLine(cursor.line);
+    const cursorListStart = cursorList.getFirstLineContentStart();
+    const lineDiff = cursor.line - cursorListStart.line;
+    const chDiff = cursor.ch - cursorListStart.ch;
+
+    return { cursorList, lineDiff, chDiff };
   }
 }
