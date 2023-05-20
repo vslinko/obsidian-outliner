3c7fc5826442a71a9bf4da91c3e559ec012ffd1a
twio142 <64556708+twio142@users.noreply.github.com>
2023-05-20 15:21:44 +0200

    Add a second test

--- src/operations/__tests__/SelectAllContent.test.ts
+++ src/operations/__tests__/SelectAllContent.test.ts
@@ -1,7 +1,7 @@
 import { makeEditor, makeRoot, makeSettings } from "../../__mocks__";
 import { SelectAllContent } from "../SelectAllContent";
 
-test("when performed the first time, should select the whole line under cursor; when performed the second time, should select all sub-bullets of the cursor line if it is a parent-bullet, otherwise select the whole list", () => {
+test("when performed the first time, should select the whole line under cursor; when performed the second time, should select all sub-bullets of the cursor line if it is a parent-bullet", () => {
   const root = makeRoot({
     editor: makeEditor({
       text: "- item 1\n- item 2\n    - item 2.1\n    - item 2.2\n- item 3\n",
@@ -23,6 +23,30 @@ test("when performed the first time, should select the whole line under cursor; 
   expect(root.getSelection().anchor.ch).toBe(2);
   expect(root.getSelection().head.ch).toBe(14);
   expect(root.getSelection().head.line).toBe(3);
+
+  op.perform();
+  expect(root.getSelection().anchor.line).toBe(0);
+  expect(root.getSelection().anchor.ch).toBe(0);
+  expect(root.getSelection().head.line).toBe(4);
+  expect(root.getSelection().head.ch).toBe(8);
+});
+
+test("when a whole line is selected and the selected line has no sub-bullets, should select the whole list", () => {
+  const root = makeRoot({
+    editor: makeEditor({
+      text: "- item 1\n- item 2\n    - item 2.1\n    - item 2.2\n- item 3\n",
+      cursor: { line: 4, ch: 2 },
+    }),
+    settings: makeSettings(),
+  });
+
+  const op = new SelectAllContent(root);
+
+  op.perform();
+  expect(root.getSelection().anchor.line).toBe(4);
+  expect(root.getSelection().anchor.ch).toBe(2);
+  expect(root.getSelection().head.line).toBe(4);
+  expect(root.getSelection().head.ch).toBe(8);
 
   op.perform();
   expect(root.getSelection().anchor.line).toBe(0);
