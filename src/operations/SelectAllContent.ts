38b0c0bc70d1f75309ce515ee88739158863ffc6
twio142 <64556708+twio142@users.noreply.github.com>
2023-05-20 04:42:57 +0200

    Fix lint; Debug; Add test

--- src/operations/SelectAllContent.ts
+++ src/operations/SelectAllContent.ts
@@ -49,7 +49,8 @@ export class SelectAllContent implements Operation {
     const contentStart = list.getFirstLineContentStartAfterCheckbox();
     const contentEnd = list.getLastLineContentEnd();
     const listUnderSelectionFrom = root.getListUnderLine(selectionFrom.line);
-    const listStart = listUnderSelectionFrom.getFirstLineContentStartAfterCheckbox();
+    const listStart =
+      listUnderSelectionFrom.getFirstLineContentStartAfterCheckbox();
     const listEnd = listUnderSelectionFrom.getContentEndIncludingChildren();
 
     this.stopPropagation = true;
@@ -58,20 +59,33 @@ export class SelectAllContent implements Operation {
     if (
       selectionFrom.line === contentStart.line &&
       selectionFrom.ch === contentStart.ch &&
-      selectionTo.line === listEnd.line &&
-      selectionTo.ch === listEnd.ch
+      selectionTo.line === contentEnd.line &&
+      selectionTo.ch === contentEnd.ch
     ) {
-      if (list.children.length) {
+      if (list.getChildren().length) {
         // select sub lists
-        root.replaceSelections([{ anchor: contentStart, head: list.getContentEndIncludingChildren() }]);
+        root.replaceSelections([
+          { anchor: contentStart, head: list.getContentEndIncludingChildren() },
+        ]);
       } else {
         // select whole list
         root.replaceSelections([{ anchor: rootStart, head: rootEnd }]);
       }
-    } else if (listStart.ch == selectionFrom.ch && listEnd.line == selectionTo.line && listEnd.ch == selectionTo.ch) {
+    } else if (
+      listStart.ch == selectionFrom.ch &&
+      listEnd.line == selectionTo.line &&
+      listEnd.ch == selectionTo.ch
+    ) {
       // select whole list
       root.replaceSelections([{ anchor: rootStart, head: rootEnd }]);
-    } else if ((selectionFrom.line > contentStart.line || (selectionFrom.line == contentStart.line && selectionFrom.ch >= contentStart.ch)) && (selectionTo.line < contentEnd.line || (selectionTo.line == contentEnd.line && selectionTo.ch <= contentEnd.ch))) {
+    } else if (
+      (selectionFrom.line > contentStart.line ||
+        (selectionFrom.line == contentStart.line &&
+          selectionFrom.ch >= contentStart.ch)) &&
+      (selectionTo.line < contentEnd.line ||
+        (selectionTo.line == contentEnd.line &&
+          selectionTo.ch <= contentEnd.ch))
+    ) {
       // select whole line
       root.replaceSelections([{ anchor: contentStart, head: contentEnd }]);
     } else {
---
+++ src/operations/__tests__/SelectAllContent.test.ts
@@ -0,0 +1,32 @@
+import { makeEditor, makeRoot, makeSettings } from "../../__mocks__";
+import { SelectAllContent } from "../SelectAllContent";
+
+test("when performed the first time, should select the whole line under cursor; when performed the second time, should select all sub-bullets of the cursor line if it is a parent-bullet, otherwise select the whole list", () => {
+  const root = makeRoot({
+    editor: makeEditor({
+      text: "- item 1\n- item 2\n    - item 2.1\n    - item 2.2\n- item 3\n",
+      cursor: { line: 1, ch: 2 },
+    }),
+    settings: makeSettings(),
+  });
+
+  const op = new SelectAllContent(root);
+
+  op.perform();
+  expect(root.getSelection().anchor.line).toBe(1);
+  expect(root.getSelection().anchor.ch).toBe(2);
+  expect(root.getSelection().head.line).toBe(1);
+  expect(root.getSelection().head.ch).toBe(8);
+
+  op.perform();
+  expect(root.getSelection().anchor.line).toBe(1);
+  expect(root.getSelection().anchor.ch).toBe(2);
+  expect(root.getSelection().head.ch).toBe(14);
+  expect(root.getSelection().head.line).toBe(3);
+
+  op.perform();
+  expect(root.getSelection().anchor.line).toBe(0);
+  expect(root.getSelection().anchor.ch).toBe(0);
+  expect(root.getSelection().head.line).toBe(4);
+  expect(root.getSelection().head.ch).toBe(8);
+});
